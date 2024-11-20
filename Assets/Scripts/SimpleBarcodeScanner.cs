ca1fe81df6b8f1269229fc50249a22259a032700
Ruchira Bogahawatta <ruchira.bogahawatta@gmail.com>
2024-11-20 23:04:32 +0530

    Product info overlay added

--- Assets/Scripts/SimpleBarcodeScanner.cs
+++ Assets/Scripts/SimpleBarcodeScanner.cs
@@ -7,10 +7,12 @@ public class SimpleBarcodeScanner : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI barcodeAsText;
     BarcodeBehaviour mBarcodeBehaviour;
-    // Start is called before the first frame update
+    public ProductInfo productInfo; // Reference to the ProductInfo script
+
     void Start()
     {
         mBarcodeBehaviour = GetComponent<BarcodeBehaviour>();
+        productInfo.ShowOverlay(false);
     }
 
 
@@ -20,11 +22,35 @@ public class SimpleBarcodeScanner : MonoBehaviour
         {
             barcodeAsText.text = mBarcodeBehaviour.InstanceData.Text;
             // Avatar
-            
+
+            // Simulated product data (replace with your API response)
+            string productName = "Product ABC";
+            string keyIngredients = "Ingredient 1, Ingredient 2";
+            string benefits = "Benefit 1, Benefit 2";
+            string sideEffects = "Side Effect 1, Side Effect 2";
+            string usage = "Twice daily";
+            string skinTypes = "All skin types";
+            string sensitivities = "None";
+            string skinConcerns = "Dryness, Acne";
+            string price = "$19.99";
+
+            // Update the UI with product information
+            productInfo.setProductInfo(
+                productName,
+                keyIngredients,
+                benefits,
+                sideEffects,
+                usage,
+                skinTypes,
+                sensitivities,
+                skinConcerns,
+                price
+            );
         }
         else
         {
             barcodeAsText.text = "";
+            productInfo.ShowOverlay(false);
         }
     }
 }
