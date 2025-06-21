dfc0d20a5fa3e8beabb564c76d8eacf67729086a
Ruchira Bogahawatta <ruchira.bogahawatta@gmail.com>
2025-06-21 22:14:28 +0530

    setting btn bug fix

--- Assets/Scripts/Auth.cs
+++ Assets/Scripts/Auth.cs
@@ -17,14 +17,16 @@ public class Auth : MonoBehaviour
     public Button loginBtn;
     public Button registerBtn;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        if (loginBtn != null) {
-            if (SessionManager.isLogged == true) {
+        if (loginBtn != null)
+        {
+            if (SessionManager.isLogged == true)
+            {
                 ToastNotification.Show("User Registered Successfully");
                 SessionManager.isLogged = false;
-            } 
+            }
             loginBtn.onClick.AddListener(Login);
         }
         if (registerBtn != null)
@@ -32,14 +34,13 @@ public class Auth : MonoBehaviour
             registerBtn.onClick.AddListener(register);
         }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void register() {
+    public void register()
+    {
         registerBtn.interactable = false;
         string fName = firstNameTxt.text;
         string lName = lastNameTxt.text;
@@ -50,7 +51,6 @@ public class Auth : MonoBehaviour
         userInfo.email = email;
         userInfo.fName = fName;
         userInfo.lName = lName;
-        //userInfo.userID = "";
         StartCoroutine(HttpUtil.Register(userInfo, OnRegisterSuccess, OnRegisterError));
 
 
@@ -58,23 +58,27 @@ public class Auth : MonoBehaviour
 
     public void Login()
     {
+        loginBtn.interactable = false;
         if (!string.IsNullOrEmpty(SessionManager.baseURL) || SessionManager.baseURL == "Base URL is not updated")
         {
-            loginBtn.interactable = false;
 
             string loginEmail = loginEmailTxt.text;
             if (!string.IsNullOrWhiteSpace(loginEmail))
             {
                 StartCoroutine(HttpUtil.Login(loginEmail, OnLoginSuccess, OnLoginError));
             }
-            else { 
-              ToastNotification.Show("Enter a proper email");
+            else
+            {
+                ToastNotification.Show("Enter a proper email");
+                loginBtn.interactable = true;
+
 
             }
         }
         else
         {
             ToastNotification.Show("Please set the Base URL first");
+            loginBtn.interactable = true;
         }
 
     }
@@ -95,7 +99,8 @@ public class Auth : MonoBehaviour
 
     private void OnLoginSuccess(UserInfo userInfo)
     {
-        if (userInfo != null) {
+        if (userInfo != null)
+        {
             SceneChange sceneChange = FindObjectOfType<SceneChange>();
             SessionManager.UserID = userInfo.userID;
             SessionManager.FirstName = userInfo.fName;
@@ -104,7 +109,8 @@ public class Auth : MonoBehaviour
 
             sceneChange.ChangeScene("Welcome Screen");
         }
-        else {
+        else
+        {
             loginEmailTxt.text = "";
             ToastNotification.Show("Invalid Email");
         }
@@ -114,6 +120,7 @@ public class Auth : MonoBehaviour
 
     private void OnLoginError(string error)
     {
+        loginBtn.interactable = true;
         Debug.Log("Error occurred: " + error);
     }
 
